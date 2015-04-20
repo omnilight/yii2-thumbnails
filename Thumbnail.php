ff8b1b1fb224916801cf4d7ddedd83dc51d2de91
Pavel <pavel.agalecky@gmail.com>
2015-04-20 18:44:14 +0300

    Now all images will be cached in the assets folder instead of the media

--- Thumbnail.php
+++ Thumbnail.php
@@ -3,6 +3,7 @@
 namespace omnilight\thumbnails;
 
 use Imagine\Image\ImageInterface;
+use Imagine\Image\ManipulatorInterface;
 use yii\base\Component;
 use yii\helpers\FileHelper;
 
@@ -15,38 +16,55 @@ class Thumbnail extends Component
     /**
      * @var string Cache path for thumbnails
      */
-    public $cachePath = '@webroot/media/thumbnails';
+    public $cachePath = '@webroot/assets';
     /**
      * @var string Cache url for thumbnails
      */
-    public $cacheUrl = '@web/media/thumbnails';
+    public $cacheUrl = '@web/assets';
 
     /**
-     * @param string $image
-     * @param callable $callable
-     * @param string $name
-     * @return string
+     * @param string $image Image file to be processed
+     * @param callable $callable Function that will process transformations.
+     * @param string $name Name of the cache, that will help to differentiate this thumbnail from the others
+     * @return string Url of the thumbnail
      */
     public function url($image, $callable, $name = '')
     {
         if (!file_exists($image))
             return '';
 
-        $cacheImageName = md5(serialize([$image, $name])) . '.' . pathinfo($image, PATHINFO_EXTENSION);
-        $cachePath = \Yii::getAlias($this->cachePath);
-        $cacheFile = $cachePath . '/' . $cacheImageName;
-        $cacheUrl = \Yii::getAlias($this->cacheUrl) . '/' . $cacheImageName;
+        $dir = md5(serialize([dirname($image), $name]));
+        $fileName = basename($image);
+        $cacheDir = \Yii::getAlias($this->cachePath) . DIRECTORY_SEPARATOR . $dir;
+        $cacheFile = $cacheDir .DIRECTORY_SEPARATOR . $fileName;
 
         $createThumbnail = !file_exists($cacheFile) || (filemtime($cacheFile) < filemtime($image));
 
         if ($createThumbnail) {
             /** @var ImageInterface $imaging */
             $imaging = call_user_func($callable, $image);
-            if (!is_dir($cachePath))
-                FileHelper::createDirectory($cachePath);
+            if (!is_dir($cacheDir))
+                FileHelper::createDirectory($cacheDir);
             $imaging->save($cacheFile);
         }
 
-        return $cacheUrl;
+        return \Yii::getAlias($this->cacheUrl . "/{$dir}/{$fileName}");
+    }
+
+    /**
+     * Creates thumbnail function callback for [[url]] function. Example:
+     * ```php
+     *  (new Thumbnail())->url($image, Thumbnail::thumb(100, 100), '100x100');
+     * ```
+     * @param integer $width
+     * @param integer $height
+     * @param string $mode
+     * @return callable
+     */
+    public static function thumb($width, $height, $mode = ManipulatorInterface::THUMBNAIL_INSET)
+    {
+        return function($image) use ($width, $height, $mode) {
+            return \yii\imagine\Image::thumbnail($image, $width, $height, $mode);
+        };
     }
 } 
